7f255356e219d71378d883b31f5d96edfabd3d5a
fmkwebdev <fmkwebdev@gmail.com>
2022-12-23 19:06:44 +0100

    asd

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -12,8 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
-import dj_database_
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -127,7 +127,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-django_heroku.settings(locals())
+
 
 MEDIA_URL = '/images/'
 
